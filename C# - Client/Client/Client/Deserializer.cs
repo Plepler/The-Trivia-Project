5b154eb656feaba78c5709646536e9ef08b6cfc8
Jplepler <Jplepler@gmail.com>
2020-07-01 20:10:49 +0300

    Started client

---
+++ C# - Client/Client/Client/Deserializer.cs
@@ -0,0 +1,162 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class RoomData
+{
+
+    public uint id;
+    public string name;
+    public uint maxPlayers;
+    public uint timePerQuestion;
+    public uint isActive;
+    public uint numOfQuestions;
+
+    public RoomData(uint id, string name, uint maxPlayers, uint timePerQuestion, uint isActive, uint numOfQuestions)
+    {
+        this.id = id;
+        this.name = name;
+        this.maxPlayers = maxPlayers;
+        this.timePerQuestion = timePerQuestion;
+        this.isActive = isActive;
+        this.numOfQuestions = numOfQuestions;
+    }
+}
+public class ErrorResponse
+{
+	public string data;
+
+    public ErrorResponse(string data)
+    {
+        this.data = data;
+    }
+}
+//Login responses
+public class LoginResponse
+{
+	public uint status;
+
+    public LoginResponse(uint status)
+    {
+        this.status = status;
+    }
+}
+public class SignupResponse
+{
+	public uint status;
+
+    public SignupResponse(uint status)
+    {
+        this.status = status;
+    }
+}
+public class LogoutResponse
+{
+	public uint status;
+
+    public LogoutResponse(uint status)
+    {
+        this.status = status;
+    }
+}
+//Room responses
+public class GetRoomsResponse
+{
+	public uint status;
+	public Queue<RoomData> rooms;
+
+    public GetRoomsResponse(uint status, Queue<RoomData> rooms)
+    {
+        this.status = status;
+        this.rooms = rooms;
+    }
+}
+public class GetPlayersInRoomResponse
+{
+	public Queue<string> players;
+
+    public GetPlayersInRoomResponse(Queue<string> players)
+    {
+        this.players = players;
+    }
+}
+public class GetStatisticsResponse
+{
+	public uint status;
+	public Queue<string> statistics;
+
+    public GetStatisticsResponse(uint status, Queue<string> statistics)
+    {
+        this.status = status;
+        this.statistics = statistics;
+    }
+}
+public class JoinRoomResponse
+{
+	public uint status;
+
+    public JoinRoomResponse(uint status)
+    {
+        this.status = status;
+    }
+}
+public class CreateRoomResponse
+{
+	public uint status;
+
+    public CreateRoomResponse(uint status)
+    {
+        this.status = status;
+    }
+}
+
+
+namespace Client
+{
+    class Deserializer
+    {
+        //Deserialize, Deserialize, Deserialize.....
+        //From byte array to json...
+        //and from json to a response class...
+        public ErrorResponse DeserializeErrorResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<ErrorResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+		public LoginResponse DeserializeLoginResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<LoginResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+		public SignupResponse DeserializeSignupResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<SignupResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+		public LogoutResponse DeserializeLogoutResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<LogoutResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+		public GetRoomsResponse DeserializeGetRoomResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<GetRoomsResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+		public GetPlayersInRoomResponse DeserializeGetPlayersResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<GetPlayersInRoomResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+		public GetStatisticsResponse DeserializeResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<GetStatisticsResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+		public JoinRoomResponse DeserializeJoinRoomResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<JoinRoomResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+		public CreateRoomResponse DeserializeCreateRoomResponse(byte[] buffer)
+		{
+            return JsonConvert.DeserializeObject<CreateRoomResponse>(Encoding.UTF8.GetString(buffer, 0, buffer.Length));
+        }
+    }
+}
---
+++ C# - Client/Client/Client/Serializer.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// All Response classes
+/// have necceseery members 
+/// and basic constructors
+/// 
+/// In: Appropriate request
+/// Out: byte buffer
+/// 
+/// </summary>
+
+
+public class SignupRequest
+{
+
+    public string username;
+    public string password;
+    public string email;
+
+    public SignupRequest(string username, string password, string email)
+    {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
+public class LoginRequest
+{
+    public string username;
+    public string password;
+
+    public LoginRequest(string username, string password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+}
+public class GetPlayersInRoomRequest
+{
+    public uint roomId;
+
+    public GetPlayersInRoomRequest(uint roomId)
+    {
+        this.roomId = roomId;
+    }
+}
+public class JoinRoomRequest
+{
+    public uint roomId;
+
+    public JoinRoomRequest(uint roomId)
+    {
+        this.roomId = roomId;
+    }
+}
+public class CreateRoomRequest
+{
+    public string roomName;
+    public uint maxUsers;
+    public uint questionCount;
+    public uint answerTimeout;
+
+    public CreateRoomRequest(string roomName, uint maxUsers, uint questionCount, uint answerTimeout)
+    {
+        this.roomName = roomName;
+        this.maxUsers = maxUsers;
+        this.questionCount = questionCount;
+        this.answerTimeout = answerTimeout;
+    }
+}
+
+
+
+namespace Client
+{
+    public static class Serializer
+    {
+        public static byte[] SerializeResponse(SignupRequest request)
+        {
+            return Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(request));
+        }
+        public static byte[] SerializeResponse(LoginRequest request)
+        {
+            return Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(request));
+        }
+        public static byte[] SerializeResponse(GetPlayersInRoomRequest request)
+        {
+            return Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(request));
+        }
+        public static byte[] SerializeResponse(JoinRoomRequest request)
+        {
+            return Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(request));
+        }
+        public static byte[] SerializeResponse(CreateRoomRequest request)
+        {
+            return Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(request));
+        }
+    }
+}
